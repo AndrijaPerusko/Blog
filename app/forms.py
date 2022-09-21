286a6fb461ef653d35c4431a0c6e417f468ce4a8
Andrija Perusko <andrija.perusko@gmail.com>
2022-09-21 18:18:47 +0200

    Avatars and Errors v3.0

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Email, ValidationError, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from app.models import User
+from flask_login import current_user
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -25,3 +26,14 @@ class RegisterForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('Please choose another email!')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user:
+                raise ValidationError('Please use a different username!')
