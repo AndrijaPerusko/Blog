49e4107efcd1aca2535b58393c5d3cdb9e3d3649
Andrija Perusko <andrija.perusko@gmail.com>
2022-09-22 18:29:20 +0200

    Pagination v5.0

--- app/forms.py
+++ app/forms.py
@@ -40,3 +40,7 @@ class EditProfileForm(FlaskForm):
 
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[DataRequired()])
+    submit = SubmitField('Submit')
