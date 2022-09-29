727959e35ab6297c894b4c20ef9e8662dbef8961
Andrija Perusko <andrija.perusko@gmail.com>
2022-09-29 17:27:01 +0200

    Bootstrap and time v7.0

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from ensurepip import bootstrap
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -7,6 +8,8 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 
 app = Flask(__name__)
@@ -16,6 +19,8 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
+moment=Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
