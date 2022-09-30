2cd303b33979675e7e04ec8f189647506ed07cad
Andrija Perusko <andrija.perusko@gmail.com>
2022-09-30 18:52:17 +0200

    Blueprints v8.0

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired, EqualTo, Email, ValidationError, Length
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from app.models import User
-from flask_login import current_user
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -26,24 +25,6 @@ class RegisterForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('Please choose another email!')
-
-class EditProfileForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
-    submit = SubmitField('Submit')
-
-    def validate_username(self,username):
-        if username.data != current_user.username:
-            user = User.query.filter_by(username=self.username.data).first()
-            if user:
-                raise ValidationError('Please use a different username!')
-
-class EmptyForm(FlaskForm):
-    submit = SubmitField('Submit')
-
-class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[DataRequired()])
-    submit = SubmitField('Submit')
 
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
---
+++ app/main/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from flask_login import current_user
+from app.models import User
+from wtforms.validators import DataRequired, Length, ValidationError
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user:
+                raise ValidationError('Please use a different username!')
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[DataRequired()])
+    submit = SubmitField('Submit')
